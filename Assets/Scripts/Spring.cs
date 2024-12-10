46c230e1b8659adf8d48e8c69bf7631a5f8a5d3b
NHWallace <54483659+NHWallace@users.noreply.github.com>
2024-12-10 15:23:57 -0800

    make spring more consistent, finish level 3 design

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -6,6 +6,7 @@ public class Spring : MonoBehaviour {
     [SerializeField] public float springForce = 40;
     private bool shouldApplyImpulse;
     private PlayerController player;
+    private float timeSinceLastImpulse = 0;
     private void OnCollisionEnter(Collision collision) {
         player = collision.gameObject.GetComponent<PlayerController>();
 
@@ -18,12 +19,21 @@ public class Spring : MonoBehaviour {
 
     private void FixedUpdate() {
         // Forces should only be applied to rigid bodies in fixed update
+        timeSinceLastImpulse += Time.fixedDeltaTime;
+
+        // Do not apply force from a spring more than once every half second
+        // This should prevent players from gaining more than the intended height
+        if (timeSinceLastImpulse < 0.5) {
+            return;
+        }
+
         if (shouldApplyImpulse) {
             Debug.Log("Impulse should be applied this fixed update frame!");
             if (player != null) {
                 Debug.Log("Forced applied!");
-                player.rb.AddForce(Vector3.up * springForce, ForceMode.Impulse);
+                player.rb.AddForce(Vector3.up * springForce, ForceMode.VelocityChange);
                 shouldApplyImpulse = false;
+                timeSinceLastImpulse = 0;
             }
         }
     }
